6477ef74b257eed81ab578471267fbfb75c00ea1
Luan Farias <matheus.frs22123@outlook.com>
2020-06-18 17:52:46 -0300

    Now you can edit a todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -31,21 +31,45 @@ class TodoController extends Controller
             ]);
 
             return redirect()->route('todo.index');
-        } else {
-            return redirect()
-                ->route('todo.add')
-                ->with("warning", "You don't have send a title");
         }
+        return redirect()
+            ->route('todo.add')
+            ->with("warning", "You don't have send a title");
     }
 
-    public function update(Request $request, $id)
+    public function update($id)
     {
-        return view('todo.update');
+        $data = DB::select("SELECT * FROM todos WHERE id = :id", [
+            'id' => $id
+        ]);
+
+        if (count($data) > 0) {
+            return view('todo.update', [
+                'data' => $data[0]
+            ]);
+        }
+        return redirect()->route('todo.index');
     }
 
     public function updateAction(Request $request, $id)
     {
+        if ($request->filled('title')) {
+            $data = DB::select("SELECT * FROM todos WHERE id = :id", [
+                'id' => $id
+            ]);
 
+            if (count($data) > 0) {
+                $title = $request->input('title');
+                DB::update("UPDATE todos SET title = :title WHERE id = :id", [
+                    'title' => $title,
+                    'id' => $id
+                ]);
+            }
+            return redirect()->route('todo.index');
+        }
+        return redirect()
+            ->route('todo.edit', ['id' => $id])
+            ->with("warning", "You don't have send a title");
     }
 
     public function delete(Request $request, $id)
--- resources/views/todo/update.blade.php
+++ resources/views/todo/update.blade.php
@@ -2,5 +2,24 @@
 @section('title', 'Edit of Todo')
 
 @section('content')
+    <h1>Edit a Todo</h1>
 
+    @if (session('warning'))
+        <x-alert>
+            @slot('type')
+                Error
+            @endslot
+            {{ session('warning') }}
+        </x-alert>
+    @endif
+
+    <form method="post">
+        @csrf
+        <label>
+            Title: <br />
+            <input type="text" name="title" value="{{$data->title}}" />
+        </label>
+
+        <input type="submit" value="Update Todo" />
+    </form>
 @endsection
